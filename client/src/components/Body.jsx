167d43c20068fa9d254945cdfe55d786afff87de
shrikantg199 <shrikantg199@gmail.com>
2024-03-18 18:14:21 +0530

    initial commit

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Feed from "./Feed";
+import Profile from "./Profile";
+import Bookmark from "./Bookmark";
+createBrowserRouter;
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
+        { path: "/", element: <Feed /> },
+        { path: "/profile", element: <Profile /> },
+        { path: "/bookmark", element: <Bookmark /> },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
